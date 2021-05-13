8d5ce3e6dc45709156fbbcab54d2320105f87956
JackieXu <343141195@qq.com>
2021-05-13 13:29:11 +0800

    client

--- src/main/java/com/jackie/investor/controller/InvestorController.java
+++ src/main/java/com/jackie/investor/controller/InvestorController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created with IntelliJ IDEA
@@ -15,7 +16,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
  * @author xujj
  * @date 2021/4/16
  */
-@Controller
+@RestController
 @RequestMapping("investor")
 public class InvestorController {
     @Autowired
